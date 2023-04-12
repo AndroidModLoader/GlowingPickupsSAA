dbbb69b6a9789d28e7e7e9296f229caf15f3988d
RusJJ <mctesting1@yandex.ru>
2023-04-12 07:41:54 +0300

    Started to work on all pickups, just saving this

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <mod/amlmod.h>
 #include <mod/logger.h>
 #include <mod/config.h>
-#include <dlfcn.h>
+#include <unordered_map>
 
 #include <GTASA_STRUCTS.h>
 
@@ -15,6 +15,31 @@ NEEDGAME(com.rockstargames.gtasa)
 // Savings
 uintptr_t pGTASA;
 void* hGTASA;
+std::unordered_map<uint16_t, CRGBA> PickupColorsMap {
+    {},
+};
+namespace PickupColors
+{
+    const CRGBA Default(0x80, 0x80, 0x80);
+    const CRGBA Melee(0x61, 0xC2, 0xF7);
+    const CRGBA Throwable(0x1B, 0x59, 0x82);
+    const CRGBA Handgun(0x95, 0xC2, 0x18);
+    const CRGBA Shotgun(0x2D, 0x9B, 0x5A);
+    const CRGBA Submachine(0xFF, 0xE3, 0x4F);
+    const CRGBA Assault(0xFE, 0x89, 0x00);
+    const CRGBA Sniper(0xF9, 0x83, 0xD7);
+    const CRGBA Heavy(0xA4, 0x28, 0xB2);
+    const CRGBA Misc(0x45, 0x45, 0x45);
+    const CRGBA Health(0xFF, 0x64, 0x64);
+    const CRGBA Armour(0x80, 0xFF, 0x80);
+    
+    inline CRGBA FindFor(uint16_t modelIdx)
+    {
+        auto it = PickupColorsMap.find(modelIdx);
+        if(it == PickupColorsMap.end()) return PickupColors::Default;
+        return it->second;
+    }
+};
 
 // Game Vars
 CCamera* TheCamera;
@@ -87,6 +112,30 @@ void DoPickupGlowing(CPickup* pu)
             
             if(glowStage2 == 3) glowInner = (rand() & 3) != 0;
             else if(glowStage2 > 3) glowInner = true;
+            
+            CRGBA clr = PickupColors::FindFor(pu->m_pObject->m_nModelIndex);
+            
+            if(glowOuter)
+            {
+                RegisterCorona((unsigned int)(pu->m_pObject), NULL, (uint8_t)(clr.r * 0.45f), (uint8_t)(clr.g * 0.45f), (uint8_t)(clr.b * 0.45f), 255, pu->m_pObject->GetPosition(),
+                               0.6f, 65.0f, PICKUP_MINE_INACTIVE, FLARETYPE_NONE, false, false, 0, 0.0f, false, -0.4f, 0, 15.0f, false, true);
+                StoreStaticShadow((unsigned int)(pu->m_pObject), 2, *gpShadowExplosionTex, &pu->m_pObject->GetPosition(), 2.0f, 0, 0, -2.0f, 0x10,
+                                  (uint8_t)(clr.r * 0.3f), (uint8_t)(clr.g * 0.3f), (uint8_t)(clr.b * 0.3f), 4.0f, 1.0f, 40.0f, false, 0.0f);
+            }
+            else
+            {
+                RegisterCorona((unsigned int)(pu->m_pObject), NULL, 0, 0, 0, 255, pu->m_pObject->GetPosition(),
+                               0.6f, 65.0f, PICKUP_MINE_INACTIVE, FLARETYPE_NONE, false, false, 0, 0.0f, false, -0.4f, 0, 15.0f, false, true);
+            }
+            
+            if(glowInner)
+            {
+                
+            }
+            else
+            {
+                
+            }
             
             break;
         }
